4e75f76c72502062120a29bfad44697f5f59c395
unknown <you@example.com>
2023-03-02 00:32:12 -0800

    mvc

---
+++ src/main/java/com/BikkadIT/MvcWithDatajpaApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcWithDatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcWithDatajpaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/BikkadIT/model/Student.java
@@ -0,0 +1,52 @@
+package com.BikkadIT.model;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer sid;
+	private String name;
+	private String email;
+	private int password;
+	private String address;
+	public Integer getSid() {
+		return sid;
+	}
+	public void setSid(Integer sid) {
+		this.sid = sid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getPassword() {
+		return password;
+	}
+	public void setPassword(int password) {
+		this.password = password;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "Student [sid=" + sid + ", name=" + name + ", email=" + email + ", password=" + password + ", address="
+				+ address + "]";
+	}
+}
